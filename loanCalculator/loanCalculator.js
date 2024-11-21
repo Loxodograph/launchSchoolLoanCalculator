16e7cced26745ba1678907ef4dd9b57bf5a32089
Nicholas <43795440+Loxodograph@users.noreply.github.com>
2024-11-21 14:08:16 +0000

    created calculator functionality

--- loanCalculator/loanCalculator.js
+++ loanCalculator/loanCalculator.js
@@ -1,1 +1,74 @@
 const MESSAGES = require('./loan_calculator_messages.json');
+const READLINE = require('readline-sync');
+let language = "en";
+
+function chooseLanguage() {
+  prompt("languageChoice");
+  let choiceOfLanguage = READLINE.question();
+  while (!invalidLanguage(choiceOfLanguage)) {
+    prompt("invalidLanguage");
+    choiceOfLanguage = READLINE.question();
+  }
+  language = choiceOfLanguage;
+}
+
+function invalidLanguage(languageChoice) {
+  return languageChoice === "pt" || languageChoice === "en";
+}
+
+function messages(message, lang = "en") {
+  return MESSAGES[lang][message];
+}
+
+function prompt(key, variable = "") {
+  let message = messages(key, language);
+  console.log(`=> ${message} ${variable}`);
+}
+
+function invalidNumber(number) {
+  return number.trimStart() === "" ||
+   Number.isNaN(Number(number)) ||
+   Number(number) < 0;
+}
+
+function chooseNumber() {
+  let number = READLINE.question();
+
+  while (invalidNumber(number)) {
+    prompt("invalidNumber");
+    number = READLINE.question();
+  }
+  return number;
+}
+
+//calculator
+prompt("welcome");
+prompt("emptyLine");
+chooseLanguage();
+displayCalculation(runCalculator());
+
+function runCalculator() {
+  prompt("loanAmount");
+  let loanAmount = Number(chooseNumber());
+
+  prompt("monthlyInterestRate");
+  let monthlyInterestRate = (Number(chooseNumber()) / 100) / 12;
+
+  prompt("loanDuration");
+  let loanDuration = Number(chooseNumber(), 10) * 12;
+
+  let monthlyPaymentDue = calculateLoan(
+    loanAmount, monthlyInterestRate, loanDuration);
+
+  return monthlyPaymentDue;
+}
+
+function calculateLoan(loanTotal, interestRate, duration) {
+  let monthlyPayment = loanTotal *
+  (interestRate / (1 - Math.pow((1 + interestRate), (-duration))));
+  return monthlyPayment;
+}
+
+function displayCalculation(calculation) {
+  return prompt("monthlyPayment", calculation.toFixed(2));
+}
